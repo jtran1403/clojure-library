05d5bef8d70f0efa8cbcef98ea5803d0feb5f6d6
jtran1403 <jtran1403@gmail.com>
2022-02-10 18:20:43 +0100

    destructuring and cleaning up

--- src/clojure_library/book.clj
+++ src/clojure_library/book.clj
@@ -4,51 +4,21 @@
 
 (def books (reader/load-edn "resources/books.edn"))
 
-(comment
-  (for [i (range 10)]
-    i)
-  (for [book books]
-    (clojure.pprint/pprint book))
-  (filter (fn [book] (= (:language book) :fr)) books)
-  (map :title books)
-  (reduce (fn [current_value book] (+ current_value (:pages book)))
-          0 books))
-
-
 (defn search-books-by-language [books language]
   (filter (fn [book] (= (:language book) language)) books))
 
-
-(defn- get-author [book]
-  (:author book))
+(defn- get-author [{:keys [author]}]
+  author)
 
 (comment
-  (filter #(= (:language %) :fr) books)
+  (filter (fn [{:keys [language]}] (= language :fr)) books)
   (let [herge {:fullname "Georges Remi"
                :nickname "Hergé"
                :birth    {:date    1907
                           :country "Belgique"}}
-        very-detailed-book {:comic  "Les Aventures de Tintin"
-                            :title  "Le secret de la licorne"
-                            :author herge}]
-    ;(:country (:birth (:author very-detailed-book)))
-    ;first-thread
-    (-> very-detailed-book :author :birth :country str/upper-case)
-
-    (-> 1
-        (+ 2)
-        (+ 5))
-    (+ (+ 1 2) 5)
-
-    (-> "a"
-        (str "b" "c"))
-
-    ;last-thread
-    (->> "a"
-        (str "b" "c"))
-
-    (->> books
-         (filter #(.contains (:tags %) :tragedy))
-         (map :title))
+        {:keys [author] :as _my-book} {:comic  "Les Aventures de Tintin"
+                                       :title  "Le secret de la licorne"
+                                       :author herge}]
+    (-> author :birth :country str/upper-case)
     )
   )
--- src/clojure_library/library.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure-library.library
-  (:require [clojure-library.book :as book]))
-
-(comment
-  book/books
-  book/get-author
-  )
-
